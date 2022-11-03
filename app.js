077f83f1709902407b64a44bf2e8a86f5a13bb18
bruceleejr <theyukidjr@gmail.com>
2022-11-03 19:02:16 -0300

    first

---
+++ app.js
@@ -0,0 +1,22 @@
+var btc = document.getElementById("btc-price");
+var eth = document.getElementById("eth-price");
+var usdt = document.getElementById("usdt-price");
+var bnb = document.getElementById("bnb-price");
+var doge = document.getElementById("doge-price");
+
+var liveprice = {
+  async: true,
+  scroosDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cbinancecoin%2Cdogecoin&vs_currencies=usd",
+
+  method: "GET",
+  headers: {},
+};
+
+$.ajax(liveprice).done(function (response) {
+  btc.innerHTML = response.bitcoin.usd;
+  eth.innerHTML = response.ethereum.usd;
+  usdt.innerHTML = response.tether.usd;
+  bnb.innerHTML = response.binancecoin.usd;
+  doge.innerHTML = response.dogecoin.usd;
+});
