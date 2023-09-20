1f11aff5b542029b3df8319fd8000e20069228e6
bruceleejr <theyukidjr@gmail.com>
2023-09-20 15:48:01 -0300

    changed colors, added refresh function, removed unnecessary elements, updated readme

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var btc = document.getElementById("btc-price");
-var eth = document.getElementById("eth-price");
-var usdt = document.getElementById("usdt-price");
-var bnb = document.getElementById("bnb-price");
-var doge = document.getElementById("doge-price");
+const btc = document.getElementById("btc-price");
+const eth = document.getElementById("eth-price");
+const usdt = document.getElementById("usdt-price");
+const bnb = document.getElementById("bnb-price");
+const doge = document.getElementById("doge-price");
+const text_logo = document.getElementById("logo");
 
+// click event on the logo to reload the page
+const reloadPage = ()=> window.location.reload();
+text_logo.addEventListener("click", reloadPage);
+
+// Fetching the data from the API
 fetch(
   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cbinancecoin%2Cdogecoin&vs_currencies=usd"
 )
   .then((response) => response.json())
-  .then(function (response) {
+  .then((response) => {
     btc.textContent = response.bitcoin.usd;
     eth.textContent = response.ethereum.usd;
     usdt.textContent = response.tether.usd;
